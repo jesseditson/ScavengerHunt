295ce7bbcd012c22dd432842c862090711ab667a
Jesse Ditson <jesse@jesseditson.com>
2011-07-10 23:25:26 -0700

    Added server, user registration and login, sessions, logout, db, other stuff

---
+++ js/models.js
@@ -0,0 +1,152 @@
+var crypto = require('crypto'),
+    Task,
+    User,
+    LoginToken;
+
+function extractKeywords(text) {
+  if (!text) return [];
+
+  return text.
+    split(/\s+/).
+    filter(function(v) { return v.length > 2; }).
+    filter(function(v, i, a) { return a.lastIndexOf(v) === i; });
+}
+
+function defineModels(mongoose, fn, secret) {
+  var Schema = mongoose.Schema,
+      ObjectId = Schema.ObjectId;
+	/**
+	* Model: Subtask
+	*/
+	Subtasks = new Schema({
+		'description':String,
+		'pointValue':Number,
+		'maxCompletions':Number
+	});
+	Subtasks.virtual('id')
+	    .get(function() {
+	      return this._id.toHexString();
+	    });
+  /**
+    * Model: Task
+    */
+  Task = new Schema({
+	'taskID' : ObjectId,
+    'title': { type: String, index: true },
+    'description': String,
+    'pointValue': Number,
+    'subTasks': [Subtasks]
+  });
+
+  Task.virtual('id')
+    .get(function() {
+      return this._id.toHexString();
+    });
+
+  Task.pre('save', function(next) {
+    next();
+  });
+	/**
+	* Model: Completions
+	*/
+	Completions = new Schema({
+		'task':String,
+		'date':Date,
+		'media':[String]
+	});
+  /**
+    * Model: User
+    */
+  function validatePresenceOf(value) {
+    return value && value.length;
+  }
+
+  User = new Schema({
+    'email': { type: String, validate: [validatePresenceOf, 'an email is required'], index: { unique: true } },
+	'name' : String,
+    'hashed_password': String,
+    'salt': String,
+	'completions':[Completions],
+	'admin':Boolean
+  });
+
+  User.virtual('id')
+    .get(function() {
+      return this._id.toHexString();
+    });
+
+  User.virtual('password')
+    .set(function(password) {
+      this._password = password;
+      this.salt = this.makeSalt();
+      this.hashed_password = this.encryptPassword(password);
+    })
+    .get(function() { return this._password; });
+ 
+  User.virtual('password_confirm').set(function(password_confirm){
+		/// do nothing, throw out this value.
+  }).get(function(){ return false; });
+
+  User.method('authenticate', function(plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
+  });
+  
+  User.method('makeSalt', function() {
+    return Math.round((new Date().valueOf() * Math.random())) + '';
+  });
+
+  User.method('encryptPassword', function(password) {
+    return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
+  });
+
+  User.pre('save', function(next) {
+    if (!validatePresenceOf(this.password)) {
+      next(new Error('Invalid password'));
+	} else {
+      next();
+    }
+  });
+
+  /**
+    * Model: LoginToken
+    *
+    * Used for session persistence.
+    */
+  LoginToken = new Schema({
+    email: { type: String, index: true },
+    series: { type: String, index: true },
+    token: { type: String, index: true }
+  });
+
+  LoginToken.method('randomToken', function() {
+    return Math.round((new Date().valueOf() * Math.random())) + '';
+  });
+
+  LoginToken.pre('save', function(next) {
+    // Automatically create the tokens
+    this.token = this.randomToken();
+
+    if (this.isNew)
+      this.series = this.randomToken();
+
+    next();
+  });
+
+  LoginToken.virtual('id')
+    .get(function() {
+      return this._id.toHexString();
+    });
+
+  LoginToken.virtual('cookieValue')
+    .get(function() {
+      return JSON.stringify({ email: this.email, token: this.token, series: this.series });
+    });
+
+  mongoose.model('Task', Task);
+  mongoose.model('User', User);
+  mongoose.model('LoginToken', LoginToken);
+
+  fn();
+}
+
+exports.defineModels = defineModels;
---
+++ server.js
@@ -0,0 +1,201 @@
+/// set up global stuff (local environment only, TODO: config this in)
+require.paths.unshift('/usr/local/lib/node');
+
+var mustache = require('./js/lib/mustache.js'),
+	Mongo = require('mongoose'),
+	mongoStore = require('connect-mongodb'),
+	models = require('./js/models'),
+	express = require('express'),
+	app = express.createServer(),
+	fs = require('fs'),
+	secret = 'mjolnir';
+	
+var tmpl = {
+	compile: function (source, options) {
+        if (typeof source == 'string') {
+            return function(options) {
+                options.locals = options.locals || {};
+                options.partials = options.partials || {};
+                if (options.body) // for express.js > v1.0
+                    locals.body = options.body;
+                return mustache.to_html(
+                    source, options.locals, options.partials);
+            };
+        } else {
+            return source;
+        }
+    },
+    render: function (template, options) {
+        template = this.compile(template, options);
+        return template(options);
+    }
+};
+
+app.configure(function(){
+	app.set('db-uri', 'mongodb://localhost/ssh2012');
+	app.set('views',__dirname + '/views');
+	app.set("view options", {layout: false});
+	app.register(".html", tmpl);
+	app.use(express.bodyParser());
+	app.use(express.cookieParser());
+	app.use(express.session({ store: mongoStore(app.set('db-uri')), secret: "MjolnirTheHammer" }));
+	app.use(express.errorHandler({ dumpExceptions: true }));
+});
+
+models.defineModels(Mongo, function() {
+  app.Task = Task = Mongo.model('Task');
+  app.User = User = Mongo.model('User');
+  app.LoginToken = LoginToken = Mongo.model('LoginToken');
+  db = Mongo.connect(app.set('db-uri'));
+}, secret);
+
+function authenticateFromLoginToken(req, res, next) {
+  var cookie = JSON.parse(req.cookies.logintoken);
+
+  LoginToken.findOne({ email: cookie.email,
+                       series: cookie.series,
+                       token: cookie.token }, (function(err, token) {
+    if (!token) {
+      res.redirect('/login');
+      return;
+    }
+
+    User.findOne({ email: token.email }, function(err, user) {
+      if (user) {
+        req.session.user_id = user.id;
+        req.currentUser = user;
+
+        token.token = token.randomToken();
+        token.save(function() {
+          res.cookie('logintoken', token.cookieValue, { expires: new Date(Date.now() + 2 * 604800000), path: '/' });
+          next();
+        });
+      } else {
+        res.redirect('/login');
+      }
+    });
+  }));
+}
+
+function loadUser(req, res, next) {
+  if (req.session.user_id) {
+    User.findById(req.session.user_id, function(err, user) {
+      if (user) {
+        req.currentUser = user;
+        next();
+      } else {
+        res.redirect('/login');
+      }
+    });
+  } else if (req.cookies.logintoken) {
+    authenticateFromLoginToken(req, res, next);
+  } else {
+    res.redirect('/login');
+  }
+}
+
+function getFile(fileName,callback){
+	fs.readFile(fileName, function (err, data) {
+	  if (err) throw err;
+	  callback(data);
+	});
+}
+function renderContent(res,view,info,template){
+	getFile(app.set('views') + '/' + view + '.html', function(html){
+		if(!template) template = "index.html";
+		if(!info) info = {};
+		res.render(template,{
+			locals: info,
+			partials: {
+				content: html.toString()
+			}
+		});
+	});
+}
+
+/* USERS */
+
+app.get('/login', function(req,res){
+	renderContent(res,'login',{'flash':req.flash('error')});
+});
+app.post('/login/do',function(req,res){
+	User.findOne({ email: req.body.email }, function(err, user) {
+	    if (user && user.authenticate(req.body.password)) {
+	      req.session.user_id = user.id;
+
+	      // Remember me
+	      if (req.body.remember) {
+	        var loginToken = new LoginToken({ email: user.email });
+	        loginToken.save(function() {
+	          res.cookie('logintoken', loginToken.cookieValue, { expires: new Date(Date.now() + 2 * 604800000), path: '/' });
+	          res.redirect('/');
+	        });
+	      } else {
+	        res.redirect('/');
+	      }
+	    } else {
+	      req.flash('error', 'Incorrect credentials');
+	      res.redirect('/login');
+	    }
+	  });
+});
+app.get('/logout', loadUser, function(req, res) {
+  if (req.session) {
+    LoginToken.remove({ email: req.currentUser.email }, function() {});
+    res.clearCookie('logintoken');
+    req.session.destroy(function() {});
+  }
+  res.redirect('/');
+});
+
+app.get('/register',function(req,res){
+	renderContent(res,'register',{'flash':req.flash('error')});
+});
+app.post('/register/do',function(req,res){
+	var user = req.body;
+	function userSaveFailed(err) {
+	    req.flash('error', err || 'Account creation failed');
+	    res.redirect('/register');
+	}
+	if(user.password != user.password_confirm ) {
+		userSaveFailed('Passwords did not match');
+	} else if(user.secret_knock != secret){
+		userSaveFailed('Wrong secret');
+    }
+	var user = new User(user);
+
+  user.save(function(err) {
+    if (err) return userSaveFailed(err);
+
+    req.flash('info', 'Your account has been created');
+    //emails.sendWelcome(user);
+
+    switch (req.params.format) {
+      case 'json':
+        res.send(user.toObject());
+      break;
+
+      default:
+        req.session.user_id = user.id;
+        res.redirect('/');
+    }
+  });
+});
+
+app.get('/users',function(req,res){
+	
+});
+
+/* TASKS */
+
+/* PAGES */
+
+app.get('/', loadUser, function(req, res) {
+  
+});
+
+if (!module.parent) {
+  app.listen(80);
+  console.log('Express server listening on port %d, environment: %s', app.address().port, app.settings.env)
+  //console.log('Using connect %s, Express %s, Jade %s', connect.version, express.version, jade.version);
+}
